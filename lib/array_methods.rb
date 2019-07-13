21c8208084a3a6169751912293e63ccc18719326
peaceful-cyborg-2337 <cmajewski@wesleyan.edu>
2019-07-13 02:16:17 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,15 +1,12 @@
 def find_element_index(array, value_to_find)
+
   array.index(value_to_find)
-end
-end
-
-
 end
 
 def find_max_value(array)
-  # Add your solution here
+array.max
 end
 
 def find_min_value(array)
-  # Add your solution here
+array.min
 end
